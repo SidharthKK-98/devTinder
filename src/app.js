01913e98a93b24ee478873f8f690c376e2666d2b
SidharthKK-98 <sidharthkk98@gmail.com>
2025-07-04 22:35:24 +0530

    delete and patch apis

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ app.post("/signup",async(req,res)=>{
 
   try{
       const user=new User(req.body)
-
+    console.log(user._id);
+    
     await user.save()
     res.send("user added successfully")
   }
@@ -21,6 +22,68 @@ app.post("/signup",async(req,res)=>{
   }
 })
 
+app.get("/user",async(req,res)=>{
+    try{
+       const users= await User.findOne({emailId:req.body.emailId})
+       if(!users){
+        res.send("no user found")
+       }
+       else{
+            res.send(users)
+
+       }
+    }
+    catch(err){
+        res.send("something went wrong")
+    }
+})
+
+app.get("/feed",async(req,res)=>{
+    try{
+       const users= await User.find({})
+       if(!users){
+        res.send("no user found")
+       }
+       else{
+            res.send(users)
+
+       }
+    }
+    catch(err){
+        res.send("something went wrong")
+    }
+})
+
+app.delete("/user",async(req,res)=>{
+
+    try{
+
+        const userId=req.body.userId
+        const users=await User.findByIdAndDelete(userId) 
+        res.send("user is deleted successfully")
+
+    }
+    catch(err){
+        res.send("something went wrong")
+    }
+})
+
+app.patch("/user",async(req,res)=>{
+    console.log(req.body);
+    
+    const emailId=req.body.emailId
+    const data=req.body
+
+    try{
+
+        const updatedUser=await User.findOneAndUpdate({emailId},data,{new:true})
+        res.send(updatedUser)
+
+    }
+    catch(err){
+        res.send("something went wrong")
+    }
+})
 
 connectDB().then(
     ()=>{
