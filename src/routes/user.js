9235e91a4ef268baf2f8895ef93a9cec4d5bec48
SidharthKK-98 <sidharthkk98@gmail.com>
2025-08-01 21:56:30 +0530

    user feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ const express=require("express")
 const userRouter=express.Router()
 const {userAuth}=require("../middlewares/auth")
 const connectionRequestModel=require("../models/connectionRequest")
-
+const user=require("../models/user")
 userRouter.get("/user/request/recieved",userAuth,async(req,res)=>{
     try{
 
@@ -53,6 +53,44 @@ userRouter.get("/user/connections",userAuth,async(req,res)=>{
     }
     catch(err){
         res.status(400).send(err)
+    }
+})
+
+userRouter.get("/user/feed",userAuth,async(req,res)=>{
+    try{
+
+        const loggedInUser=req.user
+        const allUsers=await user.find()
+        
+        const connections= await connectionRequestModel.find({
+            $or:[
+                {fromUserId:loggedInUser},
+                {toUserId:loggedInUser}
+            ]
+        }).select("fromUserId toUserId")
+
+        const hideUserFromFeed=new Set()
+         
+        connections.forEach(request=>{
+            hideUserFromFeed.add(request.fromUserId.toString())
+            hideUserFromFeed.add(request.toUserId.toString())
+
+
+        })
+
+        const Users=await user.find({
+            $and:[
+                {_id:{$nin:Array.from(hideUserFromFeed)}},
+                {_id:{$ne:loggedInUser}}//if user has no connection till now
+            ]
+        }).select("firstName lastName")
+        console.log(Users);
+        
+
+        res.send(Users)
+    }
+    catch(err){
+        res.status(400).send(err.message)
     }
 })
 
